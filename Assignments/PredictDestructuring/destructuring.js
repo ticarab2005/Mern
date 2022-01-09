00815693986c74d8c1d6be5fe1114fbfdc324b04
Ticara Bell <ticarab2005@yahoo.com>
2022-01-09 05:31:51 -0600

    commit

--- Assignments/PredictDestructuring/destructuring.js
+++ Assignments/PredictDestructuring/destructuring.js
@@ -42,7 +42,7 @@ const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
 const [, first] = numbers;
 const [, , , second] = numbers;
 const [, , , , , , , , third] = numbers;
-//Predict the output
+// Predict the output
 console.log(first == second);
 // => false
 console.log(first == third);
