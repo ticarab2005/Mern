8a16f11f443dda37b0cbeb399617c7a1bd2b5fab
Ticara Bell <ticarab2005@yahoo.com>
2022-01-09 10:39:55 -0600

    commit

--- Lectures/Scope/arrowfunction.js
+++ Lectures/Scope/arrowfunction.js
@@ -2,9 +2,14 @@
 //     console.log('Hello ' + name);
 // };
 
+
 // const sayHello = function(name) {
 //     console.log('Hello ' + name);
 // }
+
+function sayHello(name) {
+    console.log('Hello ' + name);
+}
 
 const sayHello = name => {
     console.log('Hello ' + name);
