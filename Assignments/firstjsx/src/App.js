49d88515b1960c6df50f70f3f9198f02897785a5
Ticara Bell <ticarab2005@yahoo.com>
2022-01-11 22:53:47 -0600

    commit

---
+++ Assignments/firstjsx/src/App.js
@@ -0,0 +1,21 @@
+// import "./App.css";
+
+function App() {
+  const mLeft = {
+    margin: "20px",
+  }
+  return (
+    <div style={mLeft} className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <h4>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </h4>
+    </div>
+  );
+}
+
+export default App;
