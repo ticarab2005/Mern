cc0ac417abf14a3a734e693d24404ffe32583d5b
Ticara Bell <90620635+ticarab2005@users.noreply.github.com>
2022-02-03 14:59:34 -0600

    Delete Assignments/Authors directory

--- Assignments/Authors/backend/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb://127.0.0.1/fave_authors", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("yo you're connected to the mongo db!"))
-    .catch(err => console.log("beep boop bop db connection was a flop", err))
